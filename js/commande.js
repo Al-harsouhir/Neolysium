34f4730558a6d1829e59b910fcb55d4d1c802d36
Al-harsouhir <chatmimi329@gmail.com>
2025-05-24 16:37:45 +0200

    Update commande.js

--- js/commande.js
+++ js/commande.js
@@ -1,16 +1,23 @@
-document.getElementById("mon-formulaire").addEventListener("submit", function (e) {
-  e.preventDefault();
+window.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("mon-formulaire");
+  if (form) {
+    form.addEventListener("submit", function (e) {
+      e.preventDefault();
 
-  const date = new Date().toLocaleString();
+      const date = new Date().toLocaleString();
 
-  emailjs.send("service_k7boi3m", "template_mf2m5pc", {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    discord: document.getElementById("discord").value,
-    subject: document.getElementById("subject").value,
-    message: document.getElementById("message").value,
-    time: date
-  })
-  .then(() => alert("Message envoyé avec succès !"))
-  .catch((error) => console.error("Erreur EmailJS :", error));
+      emailjs.send("service_k7boi3m", "template_mf2m5pc", {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        discord: document.getElementById("discord").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+        time: date
+      })
+      .then(() => alert("Message envoyé !"))
+      .catch((error) => console.error("Erreur EmailJS :", error));
+    });
+  } else {
+    console.error("Formulaire introuvable !");
+  }
 });
